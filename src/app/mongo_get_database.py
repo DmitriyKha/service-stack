b9e565c4c488763745cfec4cbc610fa702fb9047
Dmitriy Kha <creator.dima@gmail.com>
2026-03-09 23:50:06 +0300

    +mongo replica

--- src/app/mongo_get_database.py
+++ src/app/mongo_get_database.py
@@ -3,8 +3,9 @@ from pymongo import MongoClient
 def get_database(db_name: str, username, password):
     CONNECTION_STRING = "mongodb://mongo:27017/"
 
-    client = MongoClient(f'mongodb://{username}:{password}@mongo:27017/?authSource=admin')
-
+    # client = MongoClient(f'mongodb://{username}:{password}@mongo:27017/?authSource=admin')
+    client = MongoClient(f'mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=rs0')
+    
     return client[db_name]
 
 
